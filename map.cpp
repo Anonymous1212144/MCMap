4b988af8c09c0c05bc90bc2a159b5c5e6ce743e8
Anonymous1212144 <101783700+Anonymous1212144@users.noreply.github.com>
2025-06-24 10:25:16 -0600

    Added copyright notice

--- map.cpp
+++ map.cpp
@@ -1,6 +1,5 @@
-/*
-    A program to create a PNG map from region files
-    Copyright (C) 2025  Anonymous1212144
+/*  A program to create a PNG map from region files
+    Copyright (C) 2025 Anonymous1212144
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
